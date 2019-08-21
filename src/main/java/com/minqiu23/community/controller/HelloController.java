24ede1d280a94c7533a0d61f7f1dd2a5ef3e548c
ASmallPanda <775042376@qq.com>
2019-08-21 23:13:34 +0800

    init repository

---
+++ src/main/java/com/minqiu23/community/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.minqiu23.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by Min-Qiu23 on 2019/8/21
+ */
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name , Model model){
+
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
